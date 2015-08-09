2de5de7d7c48dc75400eea4def05a5a088ef93e9
ichigo-o-re <ichigo.o.re.t.k@gmail.com>
2015-08-09 09:29:47 +0000

    add TimeConstraint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+require 'TimeConstraint'
 Rails.application.routes.draw do
+
+  get '/', to: 'tweet#index', constraints => Timeconstraint.new
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
