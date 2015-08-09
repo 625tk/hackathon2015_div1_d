8171f2c2556513aa8a41208f4c1aaf376cf85103
ichigo-o-re <ichigo.o.re.t.k@gmail.com>
2015-08-09 15:48:25 +0000

    add enc_count, rest_count

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,2 +1,8 @@
 class Reaction < ActiveRecord::Base
+  def enc_count tweet_id
+    Reaction.where(tweet_id: tweet_id, reaction_type: 1).count
+  end
+  def rest_count tweet_id
+    Reaction.where(tweet_id: tweet_id, reaction_type: 2).count
+  end
 end
